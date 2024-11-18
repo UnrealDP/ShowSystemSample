b69f88148cc87f94dfb754fda352810593c055b7
dipi0123 <dongpyo0123@gmail.com>
2024-11-18 12:35:08 +0900

    clean

--- Plugins/ShowSystem/Source/ShowSystem/Public/RunTime/ShowBase.h
+++ Plugins/ShowSystem/Source/ShowSystem/Public/RunTime/ShowBase.h
@@ -18,7 +18,9 @@ enum class EShowKeyState : uint8
     ShowKey_End UMETA(DisplayName = "ShowKey End"),
 };
 
-
+/**
+ * 다양한 Key의 데이터를 구현하면서 필히 상속해야 하는 기본 Key Struct
+ */
 USTRUCT(Atomic, BlueprintType)
 struct FShowKey
 {
@@ -36,7 +38,11 @@ public:
 };
 
 /**
- * ShowInterface
+ * 다양한 Key 데이터로 생성할 인스턴스로 각 키의 기능 구현은 UShowBase를 상속해서 구현해야함
+ * 각 키 기능을 구현하는 곳에서 오류, 리소스 없음 등으로 키 실행이 불가능하면 해당 구문에서 바로 EShowKeyState::ShowKey_End 로 키 종료 처리 해줘야 한다.
+ * 물론 키의 기능이 정상 완료되는 곳에서도 바로 EShowKeyState::ShowKey_End 로 키 종료 처리 해줘야 한다.
+ * 키가 Pause되면 TimeScale을 0으로 해서 ApplyTimeScale 의 파라미터 FinalTimeScale을 0.0f로 호출해주니 구현 필수.
+ * TODO: (DIPI) Stop은 기능이 필요할지 혹은 Cancel로 할지 아직 미정으로 Show를 활용하는 방안을 논의한 후에 결정해야함. 아마 Cancel로 결정될 것으로 예상.
  */
 UCLASS(Abstract)
 class SHOWSYSTEM_API UShowBase : public UObject, public IPooled
@@ -85,24 +91,7 @@ public:
     void BaseTick(float DeltaTime)
     {
         float ScaleDeltaTime = DeltaTime * CachedTimeScale * ShowKey->PlayRate;
-        PassedTime += ScaleDeltaTime;
-
-        // TODO: (DIPI) 키의 종류에 따라서 시간이 아닌 이벤트 같은거로 처리할 수 있음 (예시: UShowAnimStatic)
-        /*if (CachedTimeScale > 0)
-        {
-            if (Length <= PassedTime)
-            {
-                ShowKeyState = EShowKeyState::ShowKey_End;
-            }
-        }
-        else
-        {
-            if (PassedTime <= 0.0f)
-            {
-                ShowKeyState = EShowKeyState::ShowKey_End;
-            }
-        }*/
-        
+        PassedTime += ScaleDeltaTime;        
         Tick(ScaleDeltaTime, DeltaTime, PassedTime);
     }
 
@@ -193,13 +182,31 @@ public:
 
 protected:
 
+    // Key가 인스턴스 생성되면서 최초 호출되는 함수
     virtual void Initialize() PURE_VIRTUAL(UShowBase::Initialize, );
+
+    // Key가 삭제되면서 호출되는 소멸 함수
     virtual void Dispose() PURE_VIRTUAL(UShowBase::Dispose, );
-    virtual void Play() PURE_VIRTUAL(UShowBase::Play, );
-    virtual void Stop() PURE_VIRTUAL(UShowBase::Stop, );
+
+    // Key가 StartTime이 지나서 플레이 시작될 때 호출되는 함수
+    virtual void Play() PURE_VIRTUAL(UShowBase::Play, );    
+
+    // Key를 다시 플레이 하기 위해 초기화로 불리는 함수 (대부분 툴에서 호출되는 함수)
     virtual void Reset() PURE_VIRTUAL(UShowBase::Reset, );
+
+    /*
+    *   ScaleDeltaTime : Show 내부 TimeScale 적용된 시간 변화량
+        SystemDeltaTime : Owner로 부터 호출되는 시스템 시간 변화량
+        BasePassedTime : TimeScale 적용된 현재 Key의 플레이된 지난 총 시간
+    */
     virtual void Tick(float ScaleDeltaTime, float SystemDeltaTime, float BasePassedTime) PURE_VIRTUAL(UShowBase::Tick, );
+
+    // 왜부에서 TimeScale을 변화해서 실제 최종 Key에 적용되어야 하는 FinalTimeScale 값으로 호출함 (여기서 실제 시간 변화에 대한 코드 해야함)
     virtual void ApplyTimeScale(float FinalTimeScale) PURE_VIRTUAL(UShowBase::ApplyTimeScale, );
+
+    // Key를 멈출때 호출되는 함수 (이 곳에서 멈춤 처리를 해줘야 한다)
+    // TODO: (DIPI) 아직 미확정 (Cancel로 할지 Stop으로 할지 결정 필요)
+    virtual void Stop() PURE_VIRTUAL(UShowBase::Stop, );
 
 protected:
     UShowSequencer* ShowSequencerPtr = nullptr;
@@ -209,9 +216,9 @@ protected:
 
     EShowKeyState ShowKeyState = EShowKeyState::ShowKey_Wait;
 
-    float PassedTime = 0.0f;
-    float KeyTimeScale = 1.0f;
-    float CachedTimeScale = 1.0f;
+    float PassedTime = 0.0f;    // 현재 Key의 플레이된 지난 총 시간
+    float KeyTimeScale = 1.0f;  // Key의 TimeScale
+    float CachedTimeScale = 1.0f; // 계산된 적용되어야할 TimeScale (Key의 TimeScale * ShowSequencer의 TimeScale)
 };
 
     
